eb99c283470238be35eabdc4210dbddefc54db2d
Juan Aparicio <jaf0018@alu.medac.es>
2024-04-25 08:52:42 +0200

    ajustar reginstro

--- src/proyectobbddof/Registrarse.java
+++ src/proyectobbddof/Registrarse.java
@@ -9,6 +9,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+import java.awt.Font;
 
 
 
@@ -16,10 +17,10 @@ public class Registrarse extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField_1;
-    private JTextField textField_2;
-    private JTextField textField_3;
-    private JTextField textField_4;
+	private JTextField textFieldDNI;
+    private JTextField textFieldNombre;
+    private JTextField textFieldGmail;
+    private JTextField textFieldContraseña;
     private JLabel lblNewLabel_1;
     private JLabel lblNewLabel_2;
     private JLabel lblNewLabel_3;
@@ -50,58 +51,56 @@ public class Registrarse extends JFrame {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(128, 255, 128));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
 		lblNewLabel_4 = new JLabel("DNI");
-    	lblNewLabel_4.setBounds(276, 147, 28, 14);
+		lblNewLabel_4.setFont(new Font("Mongolian Baiti", Font.BOLD, 15));
+    	lblNewLabel_4.setBounds(304, 114, 56, 14);
     	getContentPane().add(lblNewLabel_4);
     	
     	lblNewLabel_3 = new JLabel("Gmail");
-    	lblNewLabel_3.setBounds(93, 147, 46, 14);
+    	lblNewLabel_3.setFont(new Font("Mongolian Baiti", Font.BOLD, 14));
+    	lblNewLabel_3.setBounds(95, 114, 48, 14);
     	getContentPane().add(lblNewLabel_3);
     	
     	lblNewLabel_2 = new JLabel("Contraseña");
-    	lblNewLabel_2.setBounds(258, 64, 62, 14);
+    	lblNewLabel_2.setFont(new Font("Mongolian Baiti", Font.BOLD, 14));
+    	lblNewLabel_2.setBounds(293, 38, 67, 14);
     	getContentPane().add(lblNewLabel_2);
     	
     	lblNewLabel_1 = new JLabel("Nombre");
-    	lblNewLabel_1.setBounds(93, 64, 46, 14);
+    	lblNewLabel_1.setFont(new Font("Mongolian Baiti", Font.BOLD, 14));
+    	lblNewLabel_1.setBounds(81, 38, 62, 14);
     	getContentPane().add(lblNewLabel_1);
     	
-    	JPanel panel = new JPanel();
-    	panel.setBackground(new Color(128, 255, 128));
-    	panel.setBounds(0, 0, 434, 24);
-    	getContentPane().add(panel);
+    	textFieldDNI = new JTextField();
+    	textFieldDNI.setForeground(new Color(255, 255, 255));
+    	textFieldDNI.setBackground(new Color(255, 255, 255));
+    	textFieldDNI.setBounds(258, 139, 131, 23);
+    	getContentPane().add(textFieldDNI);
+    	textFieldDNI.setColumns(10);
     	
-    	JLabel lblNewLabel = new JLabel("Registro de Usuario");
-    	panel.add(lblNewLabel);
+    	textFieldNombre = new JTextField();
+    	textFieldNombre.setBounds(47, 63, 131, 23);
+    	getContentPane().add(textFieldNombre);
+    	textFieldNombre.setColumns(10);
     	
-    	textField_1 = new JTextField();
-    	textField_1.setForeground(new Color(255, 255, 255));
-    	textField_1.setBackground(new Color(255, 255, 255));
-    	textField_1.setBounds(50, 64, 135, 50);
-    	getContentPane().add(textField_1);
-    	textField_1.setColumns(10);
+    	textFieldGmail = new JTextField();
+    	textFieldGmail.setBounds(47, 139, 131, 23);
+    	getContentPane().add(textFieldGmail);
+    	textFieldGmail.setColumns(10);
     	
-    	textField_2 = new JTextField();
-    	textField_2.setBounds(222, 64, 135, 50);
-    	getContentPane().add(textField_2);
-    	textField_2.setColumns(10);
-    	
-    	textField_3 = new JTextField();
-    	textField_3.setBounds(50, 147, 135, 50);
-    	getContentPane().add(textField_3);
-    	textField_3.setColumns(10);
-    	
-    	textField_4 = new JTextField();
-    	textField_4.setBounds(222, 147, 135, 50);
-    	getContentPane().add(textField_4);
-    	textField_4.setColumns(10);
+    	textFieldContraseña = new JTextField();
+    	textFieldContraseña.setBounds(258, 63, 131, 23);
+    	getContentPane().add(textFieldContraseña);
+    	textFieldContraseña.setColumns(10);
     	
     	JButton btnNewButton = new JButton("REGISTRAR USUARIO");
-    	btnNewButton.setBounds(128, 215, 157, 23);
+    	btnNewButton.setBounds(148, 200, 139, 23);
     	getContentPane().add(btnNewButton);
 	}
 

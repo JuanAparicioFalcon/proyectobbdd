8ff655fc0fb985c41a9c4102649957c3a53fdc8e
JuanCarlosCastilla <jcg0059@alu.medac.es>
2024-04-25 10:30:51 +0200

    dd

--- src/proyectobbddof/menu.java
+++ src/proyectobbddof/menu.java
@@ -1,8 +1,13 @@
 package proyectobbddof;
 
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
@@ -37,6 +42,16 @@ public class menu extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		
+		JLabel lblFondo = new JLabel("");
+		lblFondo.setForeground(new Color(0, 128, 0));
+		lblFondo.setBounds(0, 0, 1980, 1080);
+		lblFondo.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		contentPane.add(lblFondo);
+		ImageIcon icono2=new ImageIcon(InicioSesion.class.getResource("/img/fondo.jpg"));
+				Image imagen2 =	icono2.getImage().getScaledInstance(lblFondo.getWidth(),lblFondo.getHeight(), Image.SCALE_SMOOTH);
+				ImageIcon iconoAjustado2 = new ImageIcon(imagen2);
+				lblFondo.setIcon(iconoAjustado2);
 	}
 
 }

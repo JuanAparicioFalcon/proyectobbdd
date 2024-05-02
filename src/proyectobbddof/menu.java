ede98ad9bbac783cc7237c512ffc104eb14c5d79
JuanCarlosCastilla <jcg0059@alu.medac.es>
2024-05-02 09:25:21 +0200

    ppp

--- src/proyectobbddof/menu.java
+++ src/proyectobbddof/menu.java
@@ -19,6 +19,9 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JCheckBox;
+import javax.swing.JList;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 
 public class menu extends JFrame {
@@ -58,12 +61,34 @@ public class menu extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_1.setBounds(1217, 600, 89, 23);
+		contentPane.add(btnNewButton_1);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(962, 240, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(1088, 176, 218, 69);
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Seleccionar:", "Nombre", "Posicion", "Edad", "Categoria", "Nacionalidad"}));
+		contentPane.add(comboBox);
+		
+		JButton btnNewButton = new JButton("Filtrador Cantera");
+		btnNewButton.setBounds(683, 117, 623, 61);
+		btnNewButton.setBackground(new Color(255, 255, 255));
+		contentPane.add(btnNewButton);
+		
 		JLabel lblFondo = new JLabel("");
-		lblFondo.setForeground(new Color(0, 128, 0));
 		lblFondo.setBounds(0, 0, 1980, 1080);
+		lblFondo.setForeground(new Color(0, 128, 0));
 		lblFondo.setFont(new Font("Times New Roman", Font.PLAIN, 18));
 		contentPane.add(lblFondo);
-		ImageIcon icono2=new ImageIcon(InicioSesion.class.getResource("/img/fondo.jpg"));
+		ImageIcon icono2=new ImageIcon(menu.class.getResource("/img/fondo.jpg"));
 				Image imagen2 =	icono2.getImage().getScaledInstance(lblFondo.getWidth(),lblFondo.getHeight(), Image.SCALE_SMOOTH);
 				ImageIcon iconoAjustado2 = new ImageIcon(imagen2);
 				lblFondo.setIcon(iconoAjustado2);
